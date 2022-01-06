f804b84e9d6453638e7772ce1faed98a03d4f590
AzDan <vrocks98@gmail.com>
2022-01-06 20:54:27 +0530

    moved fetch req to functions, function to fetch all data, storing servertime as number in state

--- client/src/App.js
+++ client/src/App.js
@@ -1,41 +1,62 @@
-import React from "react";
+import React, { useCallback } from "react";
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
 
-  const [data, setData] = React.useState(null);
+  const [data, setData] = React.useState(0);
   const [symbol, setSymbol] = React.useState("btcinr");
   const [ticker, setTicker] = React.useState(null);
+  const [allValues, setAllValues] = React.useState([]);
 
-  React.useEffect(() => {
-    fetch("/time")
+  const getTime = useCallback(
+    () => {
+      fetch("/time")
       .then((res) => res.json())
       .then((data) => {
-        setData(JSON.stringify(data))
+        setData(data.serverTime)
       });
-  }, []);
-
-  React.useEffect(() => {
-    fetch(`/ticker/${symbol}`)
+    },
+    [],
+  );
+  
+  const getCurrentCryptoValue = useCallback(
+    () => {
+      fetch(`/ticker/${symbol}`)
       .then((res) => res.json())
       .then((data) => {
         console.log(data);
         setTicker(data);
       });
-  }, []);
-
+    },
+    [symbol],
+  );
+  
+  const getAllCryptoValues = useCallback(
+    () => {
+      fetch(`/alldata`)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setAllValues(data);
+      });
+    },
+    [],
+  );
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <button type="button" onClick={getTime}>Get Time</button>
         <p>
           {!data ? "loading" : data}
         </p>
+        <button onClick={getCurrentCryptoValue}>Get Current Crypto</button>
         <p>
           {!symbol ? "loading" : symbol}
         </p>
+        <button onClick={getAllCryptoValues}>Get all values</button>
         <p>
           {!ticker ? "loading" : JSON.stringify(ticker)}
         </p>
